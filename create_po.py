94b4a32b59bceffa7148518477a4f94d794d5873
alerion <alerion.um@gmail.com>
2012-02-15 16:16:13 +0200

    add locale files

---
+++ create_po.py
@@ -0,0 +1,17 @@
+import os
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+LOCALE_PATH = os.path.join(BASE_PATH, 'locale/ru/LC_MESSAGES')
+TRANSLATION_PATH = os.path.join(BASE_PATH, '_build/translation')
+
+for path, dirs, files in os.walk(TRANSLATION_PATH):
+    for f in files:
+        if f.endswith('.pot'):
+            pot_path = os.path.join(path, f)
+            p = pot_path[len(TRANSLATION_PATH) + 1:-1]
+            po_path = os.path.join(LOCALE_PATH, p)
+
+            if not os.path.exists(os.path.dirname(po_path)):
+                os.makedirs(os.path.dirname(po_path))
+
+            os.system('msginit -i %s -o %s -l ru --no-translator' % (pot_path, po_path))
