99931c1d416461db2dd9fc6845db14a95b0ee050
Alerion <alerion.um@gmail.com>
2013-08-20 12:21:39 +0300

    fix authors command

--- djbook/commands/authors.py
+++ djbook/commands/authors.py
@@ -1,9 +1,11 @@
 from cliff.command import Command as BaseCommand
 from jinja2 import Environment, FileSystemLoader
-from github import Github
+from github import Github, GithubException
 import logging
 import os
 import getpass
+
+PASS_CHACHE_FILE = '.github.auth'
 
 
 class Command(BaseCommand):
@@ -13,9 +15,26 @@ class Command(BaseCommand):
     log = logging.getLogger(__name__)
 
     def take_action(self, parsed_args):
-        print 'We need authenticate your in Github for Search API.'
-        github_username = raw_input('Github username: ')
-        github_password = getpass.getpass('Github password: ')
+        github_username, github_password = None, None
+
+        try:
+            with open(PASS_CHACHE_FILE) as f:
+                data = f.read()
+                if data:
+                    github_username, github_password = data.split('\n')
+        except IOError as e:
+            pass
+
+        if not github_username or not github_password:
+            print 'We need authenticate your in Github for Search API.'
+            github_username = raw_input('Github username: ')
+            github_password = getpass.getpass('Github password: ')
+
+            with open(PASS_CHACHE_FILE, 'w') as f:
+                f.write(github_username)
+                f.write('\n')
+                f.write(github_password)
+
         repo_name = 'Alerion/django_documentation'
 
         github = Github(github_username, github_password)
