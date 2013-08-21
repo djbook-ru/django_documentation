1a08eab78f9922bb18c26477965d457ad890e925
Alerion <alerion.um@gmail.com>
2013-08-21 17:04:06 +0300

    add update command

--- djbook/commands/authors.py
+++ djbook/commands/authors.py
@@ -1,44 +1,21 @@
-from cliff.command import Command as BaseCommand
-from jinja2 import Environment, FileSystemLoader
-from github import Github, GithubException
 import logging
 import os
-import getpass
 
-PASS_CHACHE_FILE = '.github.auth'
+from cliff.command import Command as BaseCommand
+from jinja2 import Environment, FileSystemLoader
+from github import GithubException
+
+from djbook.cliff_utils import GithubCommandMixin
 
 
-class Command(BaseCommand):
+class Command(GithubCommandMixin, BaseCommand):
     """
     Get authors from git history
     """
     log = logging.getLogger(__name__)
 
     def take_action(self, parsed_args):
-        github_username, github_password = None, None
-
-        try:
-            with open(PASS_CHACHE_FILE) as f:
-                data = f.read()
-                if data:
-                    github_username, github_password = data.split('\n')
-        except IOError as e:
-            pass
-
-        if not github_username or not github_password:
-            print 'We need authenticate your in Github for Search API.'
-            github_username = raw_input('Github username: ')
-            github_password = getpass.getpass('Github password: ')
-
-            with open(PASS_CHACHE_FILE, 'w') as f:
-                f.write(github_username)
-                f.write('\n')
-                f.write(github_password)
-
-        repo_name = 'Alerion/django_documentation'
-
-        github = Github(github_username, github_password)
-        repo = github.get_repo(repo_name)
+        repo = self.get_repo('Alerion/django_documentation')
 
         try:
             self.save_to_file({
