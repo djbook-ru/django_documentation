7d39dcac6574b05cbafed3f6aa95c240dc40d36e
Dmitriy Kostochko <alerion.um@gmail.com>
2014-04-09 12:44:20 +0300

    add git commit to footer

--- djbook/djbook_conf.py
+++ djbook/djbook_conf.py
@@ -3,11 +3,19 @@
 # from djbook_conf import *
 import sys
 import os
+from git import Repo
 
 #sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "_ext")))
 
 #extensions = ["djbookdocs", "sphinx.ext.intersphinx"]
 #html_translator_class = "djbookdocs.DjangoHTMLTranslator"
+
+repo = Repo('.')
+
+html_context = {
+    'git_commit': str(repo.commit()),
+    'git_branch': str(repo.active_branch)
+}
 
 html_theme_path = ["_theme", "djbook/_theme"]
 html_theme = "djbook"
