aa34abd8bbe504a988642f5848bc97da448642c0
Kostochko Dmitriy <alerion.um@gmail.com>
2014-07-14 13:21:36 +0300

    fix spellcheck and translation

--- djbook/commands/spellcheck.py
+++ djbook/commands/spellcheck.py
@@ -2,7 +2,6 @@
 import logging
 import os
 
-from shutil import copyfile
 from cliff.command import Command as BaseCommand
 
 
@@ -14,33 +13,10 @@ class Command(BaseCommand):
     log = logging.getLogger(__name__)
 
     def take_action(self, parsed_args):
-        self._check_enviroment()
+        self.app.run_subcommand(['compilemessages'])
 
         doctrees_path = os.path.join(self.app.doc_path, '_build/doctrees')
         spelling_path = os.path.join(self.app.doc_path, '_build/spelling')
         cmd = 'sphinx-build -b spelling -n -d %s -D language=ru_RU . %s'
         os.system(cmd % (doctrees_path, spelling_path))
         self.app.stdout.write('Check result in %s\n' % spelling_path)
-
-    def _check_enviroment(self):
-        import enchant
-        enchant_path = os.path.dirname(enchant.__file__)
-
-        # FIXME: dirty hack to fix tokenize fo ru_RU
-        try:
-            from enchant.tokenize.ru import tokenize
-        except ImportError:
-            copyfile(
-                os.path.join(self.app.doc_path, 'djbook/spelling_tokenize_ru.py'),
-                os.path.join(enchant_path, 'tokenize/ru.py')
-            )
-
-        from enchant.tokenize.ru import tokenize
-
-        # Check if ru_RU dictionary exists
-        if not os.path.exists(os.path.join(enchant_path, 'share/enchant/myspell/ru_RU.dic')):
-            cmd = 'mkdir -p %s/share/enchant/myspell'
-            os.system(cmd % enchant_path)
-
-            cmd = 'tar -xzf %s/djbook/dict_ru.tar.gz -C %s/share/enchant/myspell'
-            os.system(cmd % (self.app.doc_path, enchant_path))
