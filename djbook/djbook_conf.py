8421f2d17f5e0db18aeed10cae6fbc2c2224d147
Kostochko Dmitriy <alerion.um@gmail.com>
2014-10-23 18:08:07 +0300

    fix

--- djbook/djbook_conf.py
+++ djbook/djbook_conf.py
@@ -4,14 +4,10 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "_ext")))
-
-extensions = ["djbookdocs", "sphinx.ext.intersphinx"]
-html_translator_class = "djbookdocs.DjangoHTMLTranslator"
-
 html_theme_path = ["_theme", "djbook/_theme"]
 html_theme = "djbook"
 
 language = 'ru'
 locale_dirs = ['./locale/']
+exclude_patterns = ['_build', 'djbook', 'env']
 gettext_compact = False
